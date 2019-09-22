eb03ffc0c1b9434edb5f55398d976e6ea69bc2b9
linnea.nagelkerke@gmail.com <linnea.nagelkerke@gmail.com>
2019-09-22 16:39:43 +0200

    added saved dice text.

--- Game-project/main.js
+++ Game-project/main.js
@@ -3,6 +3,42 @@ var $ = function (foo) {
     return document.getElementById(foo);    // save keystrokes
 }
 
+// Create, read and erease cookies 
+function createCookie(name, value, days) {
+    let expires;
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1,c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+
+// Write a form and js functions for cookies 
+
 
 const main = function () {
 
@@ -17,10 +53,10 @@ const main = function () {
     for (let dice = 1; dice < 6; dice++) { // We create the loop so we can apply the roll function to all 5 dice.
         let currentDie = "die" + dice;  // Combining the string "die" with the amount of dice we have, instead of having to create several variables "die1, die2, die3 ..."
         let saveCurrentDie = $(currentDie).getAttribute("data-save");
-        let checkTxt = "checkConfirm" + dice;
+        let checkTxt = "checkTxt" + dice;
 
-        if (saveCurrentDie === "1") { // This will save the dice and not roll if checked.
-            $(checkTxt).innerHTML = "You have saved this die"; // ASK NIELS FOR ADVICE
+        if (saveCurrentDie === "1") { // This will stop the roll of your selected die
+             console.log("you saved die nr: " + dice); // ASK NIELS FOR ADVICE
         } else {
             $(currentDie).innerHTML = roll();
         }
@@ -30,10 +66,12 @@ const main = function () {
 const CheckSaveStates = function () {  // Save dice function
     for (let DiceNumber = 1; DiceNumber < 6; DiceNumber++) { // We create this loop to iterate through all the dice to apply the value "1" = checked.
         let savedDiceName = "saveD" + DiceNumber;
-        let currentDieChecked = "die" + DiceNumber;  // Combining the word die and with the amount of dice we have
+        let currentDieChecked = "die" + DiceNumber;  
+        let checkTxt = "checkTxt" + DiceNumber;
 
         if ($(savedDiceName).checked) {
             $(currentDieChecked).setAttribute("data-save", "1");   // This adds the value "1" to the data-save class and mark it as Checked for our script
+            $(checkTxt).innerHTML = "This die was saved";
         } else {
             $(currentDieChecked).setAttribute("data-save", "0");
         }
@@ -43,6 +81,7 @@ const CheckSaveStates = function () {  // Save dice function
 
 let init = function () {
     $('btnRoll').addEventListener('click', main);
+
 }
 
 window.addEventListener('load', init);
