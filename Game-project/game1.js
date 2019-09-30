8a993dde46899a38d852d2e4489ea577dd5c22d2
linnea.nagelkerke@gmail.com <linnea.nagelkerke@gmail.com>
2019-09-30 12:03:48 +0200

    added the right version of guessing

--- Game-project/game1.js
+++ Game-project/game1.js
@@ -11,10 +11,11 @@ var attempts = 0;
 var maxGuesses = 10;
 
 
+
 function gameEnded(){
     $('newGameBtn').style.display = 'inline';
     $("easyBtn").style.display = 'none';
-    document.getElementById('output').setAttribute('readonly', 'readonly'); // can't click on the input field
+    document.getElementById('inputGuess').setAttribute('readonly', 'readonly'); // can't click on the input field
 }
 function easyMode(){
     maxGuesses = 10;
@@ -47,10 +48,10 @@ function compareGuess() {
     if(userGuessLog.length < maxGuesses){
         if (userGuess > computerGuess) { 
             $('output').innerHTML = 'Your guess is too high.';
-            document.getElementById('output').value =""; // clear input field each time it have been guessed
+            document.getElementById('inputGuess').value =""; // clear input field each time it have been guessed
         } else if (userGuess < computerGuess) {
             document.getElementById('output').innerHTML = 'Your guess is too low.';
-            document.getElementById('output').value = "";
+            document.getElementById('inputGuess').value = "";
         } else {
             document.getElementById('output').innerHTML = 'You guessed right! you got in ' + attempts + " attempts";
             document.getElementById('container').style.backgroundColor = 'green';
@@ -75,6 +76,7 @@ function compareGuess() {
     }
    
 }
+
 
 
 
@@ -112,23 +114,53 @@ function eraseCookie(name) {
 }
 
 
+
+// function to show highscore 
+function showScore() {
+    let cookietxt = document.cookie; // reads all cookies 
+    let arr = cookietxt.split(';'); // splits all cookies elements after ; 
+    for (let i = 0; i < arr.length; i++)  { // create a loop for the whole array 
+        let tr = document.createElement('tr'); // create table row in html
+        let arr1 = arr[i].split('='); // splits name and value 
+        let td = document.createElement('td'); // creates the table data
+        let arrayTxt = document.createTextNode(arr1[0]); // array content 
+
+        // Why we don't use let here or (declare it to a new variable) is because we ABOVE declared e.g ((let td = 5;)) so if we want to change the value of the variable td later we can just
+        // write ((td = 10;))
+        td.appendChild(arrayTxt); // input table content
+        tr.appendChild(td); // input table data in to a row
+        td = document.createElement('td'); // change varible
+        arrayTxt = document.createTextNode(arr1[1]); // create value in table 
+        td.appendChild(arrayTxt);
+        tr.appendChild(td);
+        $('showScore').appendChild(tr); // input to table in html file
+    }
+}
+
+
+//This works, and saves cookies, also displays it... FIGURE OUT HOW TO DISPLAY AT ALL TIMES.
 let saveScore = function () {
     let pName = $('playerName').value;
 
     if (pName.length > 0) {
         $('playerScore').innerHTML = $('playerName').value;
-        $('playerAttempts').innerHTML = "guessed it in" + attempts;
+        $('playerAttempts').innerHTML = "guessed it in" + " " + attempts;
+        
         createCookie(pName, attempts, 14);
-    } else {
-        console.log(document.);
     }
 }
+
+
 //#endregion
 
 let windowLoad = function () {
+    
     $('newGameBtn').addEventListener('click', newGame);
     $("inputGuess").addEventListener('change', compareGuess);
     $('saveScoreToBoard').addEventListener('click', saveScore);
+    showScore();
+    // $('showCookie').textContent = document.cookie;
+    
 }
 
 window.addEventListener('load', windowLoad);
